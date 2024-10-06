20ad5e6a5e7c574ab72133081e51319f5ef8f390
Akito D. Kawamura <akito.davis@gmail.com>
2024-10-06 15:28:58 +0900

    Update csvreader.js

--- csvreader.js
+++ csvreader.js
@@ -10,7 +10,7 @@ function printon(data){
 	const articles = data.slice(1)
 		.map(x => `
 			<article>
-				<h3>${x[8]}</h3>
+				<h3>planet ${x[8]}</h3>
 				<p>${x[5]}, ${x[6]}, ${x[7]}</p>
 				<p>${parseFloat(x[5])+parseFloat(x[6])+parseFloat(x[7])} </p>
 			</article>
