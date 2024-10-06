2dc782b6481a6f26721dcfdac3019368444a0d76
Akito D. Kawamura <akito.davis@gmail.com>
2024-10-06 15:28:24 +0900

    Update csvreader.js

--- csvreader.js
+++ csvreader.js
@@ -3,16 +3,20 @@ async function loadCSVData() {
   const text = await response.text();
   const data = text.trim().split('\n')
     .map(line => line.split(',').map(x => x.trim()));
-  const articles = data.slice(1)
-    .map(x => `
-      <article>
-        <h3>${x[8]}</h3>
-        <p>${x[5]}, ${x[6]}, ${x[7]}</p>
-				<p>${parseFloat(x[5])+parseFloat(x[6])+parseFloat(x[7])} </p>
-      </article>
-    `)
-    .join(';');
-  document.getElementById('js-csv').innerHTML = articles;
+	return data;
 }
 
-loadCSVData();
+function printon(data){
+	const articles = data.slice(1)
+		.map(x => `
+			<article>
+				<h3>${x[8]}</h3>
+				<p>${x[5]}, ${x[6]}, ${x[7]}</p>
+				<p>${parseFloat(x[5])+parseFloat(x[6])+parseFloat(x[7])} </p>
+			</article>
+		`)
+		.join(';');
+	document.getElementById('js-csv').innerHTML = articles;
+}
+
+printon(loadCSVData());
